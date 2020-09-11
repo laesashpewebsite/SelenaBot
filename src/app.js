313d1654d39db77c1439991e89d1bec448a2456c
Daniel Gaston <danielgaston6@gmail.com>
2020-09-11 12:02:52 -0400

    debug mode

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const { WebClient } = require ('@slack/web-api');
+import axios from "axios";
 
-const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require("@slack/web-api");
 
-const dotenv =require("dotenv");
+const { createEventAdapter } = require("@slack/events-api");
+
+const dotenv = require("dotenv");
 dotenv.config();
 
 const web = new WebClient(process.env.SLACK_OAUTH_TOKEN);
@@ -14,99 +16,101 @@ const SelenaBotId = "U01A6KT325R";
 
 var unirest = require("unirest");
 
+let scholarships = {};
+
 var req = unirest("GET", "https://healthruwords.p.rapidapi.com/v1/quotes/");
 
 req.query({
-	"maxR": "1",
+  maxR: "1",
 });
 
 req.headers({
-	"x-rapidapi-host": "healthruwords.p.rapidapi.com",
-	"x-rapidapi-key": "3bc797064dmsh88cd64ba177bd18p15ebc0jsn41321cd9714e",
-	"useQueryString": true
+  "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+  "x-rapidapi-key": "3bc797064dmsh88cd64ba177bd18p15ebc0jsn41321cd9714e",
+  useQueryString: true,
 });
 
-slackEvents.on('app_mention', (event) => {
-
-        var tempString = event.text; 
-        tempString = tempString.toUpperCase()        
-        if(event.user !== SelenaBotId ){
-            // this is to prevent from sending messages when te bot detects itself and 
-            //when @Selena  bot is not detected
-            if(tempString.includes(`/HELP`)){
-                //ENABLE THIS COMMENT TO POST TO SLACK
-                 sendMessage(event.channel,`Can't help you yet but I can tell you, you are awesome`);
-                // console.log('in help')
-            }
-            else if(tempString.includes(`/EVENT`)  ){
-                //ENABLE THIS COMMENT TO POST TO SLACK
-                 sendMessage(event.channel, `Can't help you yet but you can ask your amazing Eboard in the meantime!`);
-                // console.log('in events');
-            }    
-            else if(tempString.includes(`/POINTS`)){}
-            else if(tempString.includes(`/MOTIVATE`)){
-                 getMotivation(event.channel)
-            }
-            else{
-                // sendMessage(event.channel,`Did you want something? would you like for me to display the help list?`);
-
-            }
-        }
-})
-eventText = (channelId,userId) =>{
-    //This function will do is it will output all the upcoming events 
-    //within a 2 week time span. Display their information accordingly like so:
-    // "Events_name is on Events_date: Events_description this event is worth this
-    // events is under Event_category so you will get Events_point if you attend"  
-}
-helpText = (channelId,userId) => {
-    //This function will output a list of potential commands so people can see how 
-    //SELENA Bot 
-}
-scholarshiptext = (channelId,userId) =>{
-    // This fucntion will provide a list of all available scholarships 
-    // and their due date:
-    //"Scholarship_name - Scholarship_info: Scholarship_dueDate"
-}
-getMotivation = (channelId) =>{
-    
-    console.log("in moti")
-    req.end(function (res) {
-        if (res.error) 
-        {
-            console.error(res.error)
-            throw new Error(res.error);
-        }
-        // var body = res.body;
-        console.log(res.body[0].media);
-        sendMessage(channelId, `Here is a motivational qoute: ${res.body[0].media}`);
-    });
-}
-
-
-const sendMessage = async (channel, message) =>{
-    
-    try{ 
-        const result = await web.chat.postMessage({
-        channel: channel,
-        text: message,
-    });
-        // console.log(`Successfully sent message ${result.ts} in conversation ${channel}`);
-
+slackEvents.on("app_mention", (event) => {
+  var tempString = event.text;
+  tempString = tempString.toUpperCase();
+  if (event.user !== SelenaBotId) {
+    // this is to prevent from sending messages when te bot detects itself and
+    //when @Selena  bot is not detected
+    if (tempString.includes(`/HELP`)) {
+      //ENABLE THIS COMMENT TO POST TO SLACK
+      sendMessage(
+        event.channel,
+        `Can't help you yet but I can tell you, you are awesome`
+      );
+      // console.log('in help')
+    } else if (tempString.includes(`/EVENT`)) {
+      //ENABLE THIS COMMENT TO POST TO SLACK
+      sendMessage(
+        event.channel,
+        `Can't help you yet but you can ask your amazing Eboard in the meantime!`
+      );
+      // console.log('in events');
+    } else if (tempString.includes(`/SCHOLARSHIP`)) {
+      scholarshiptext();
+    } else if (tempString.includes(`/MOTIVATE`)) {
+      getMotivation(event.channel);
+    } else {
+      // sendMessage(event.channel,`Did you want something? would you like for me to display the help list?`);
     }
-    catch(error){
+  }
+});
+// eventText = (channelId, userId) => {
+//   //This function will do is it will output all the upcoming events
+//   //within a 2 week time span. Display their information accordingly like so:
+//   // "Events_name is on Events_date: Events_description this event is worth this
+//   // events is under Event_category so you will get Events_point if you attend"
+// };
+// helpText = (channelId, userId) => {
+//   //This function will output a list of potential commands so people can see how
+//   //SELENA Bot
+// };
+scholarshiptext = (channelId) => {
+  axios
+    .get("https://laesa-backend.herokuapp.com/api/scholarships")
+    .then((res) => {
+      scholarships = res.data;
+      console.log(scholarships);
+    });
+};
+getMotivation = (channelId) => {
+  console.log("in moti");
+  req.end(function (res) {
+    if (res.error) {
+      console.error(res.error);
+      throw new Error(res.error);
+    }
+    // var body = res.body;
+    console.log(res.body[0].media);
+    sendMessage(
+      channelId,
+      `Here is a motivational qoute: ${res.body[0].media}`
+    );
+  });
+};
+
+const sendMessage = async (channel, message) => {
+  try {
+    const result = await web.chat.postMessage({
+      channel: channel,
+      text: message,
+    });
+    // console.log(`Successfully sent message ${result.ts} in conversation ${channel}`);
+  } catch (error) {
     console.error(error);
-    }
-    console.log("still sending");
-}
+  }
+  console.log("still sending");
+};
 
 (async () => {
-    // Start the built-in server
-    const server = await slackEvents.start(port);
-   
-    // Log a message when the server is ready
-    // getMotivation();
-    console.log(`Listening for events on ${server.address().port}`);
+  // Start the built-in server
+  const server = await slackEvents.start(port);
 
+  // Log a message when the server is ready
+  // getMotivation();
+  console.log(`Listening for events on ${server.address().port}`);
 })();
-
